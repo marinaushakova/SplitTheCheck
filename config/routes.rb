70a807154bbd292f148173fcd962c8ca1121e40c
mushako1 <mushako1@my.westga.edu>
2015-03-23 09:58:02 -0700

    added restriction on deleting and editing restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-resources :restaurants
+resources :restaurants, except: [:destroy, :edit]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
