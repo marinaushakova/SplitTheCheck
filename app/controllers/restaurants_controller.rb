7ed750a5522131bc2d39dd596a89453b00ef9953
mushako1 <mushako1@my.westga.edu>
2015-04-16 19:47:06 -0700

    fixed issue with creating new restaurant with non zero number of votes by deleting all data from Votes table (invalid data was entered when Votes table was first filled with data)

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -91,6 +91,6 @@ class RestaurantsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def restaurant_params
-      params.require(:restaurant).permit(:name, :address, :city, :state, :zip, :upvote, :downvote)
+      params.require(:restaurant).permit(:name, :address, :city, :state, :zip)
     end
 end
