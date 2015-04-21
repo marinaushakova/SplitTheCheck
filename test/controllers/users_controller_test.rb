98e00ab558d506902ce2eb81b92b456581a96e3d
mushako1 <mushako1@my.westga.edu>
2015-04-20 17:17:55 -0700

    added should get user summary test, modified fixtures to be able to test all of the functionality

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,12 +2,22 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
   setup do
-    @user = users(:one)
+    @user = User.first
+    @restaurant = restaurants(:one)
   end
 
   test "should show user" do
     sign_in @user
     get :show, id: @user
+    assert_response :success
+  end
+  
+  test "should get user summary" do
+    sign_in @user
+    get :show, id: @user
+    assert_equal 2, Vote.where(:user => User.first).count
+    assert_equal 2, Favorite.where(:user => User.first).count
+    assert_equal 1, Comment.where(:user => User.first).count
     assert_response :success
   end
 
